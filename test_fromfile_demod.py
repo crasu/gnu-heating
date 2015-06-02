6e54974b19ff89a8f586f0a82f4d21d52c9a147b
Christian Kraemer <christian.kraemer@tngtech.com>
2015-06-02 23:07:13 +0200

    hmm

--- test_fromfile_demod.py
+++ test_fromfile_demod.py
@@ -2,23 +2,20 @@ from gnuradio import gr, gr_unittest
 from gnuradio import blocks
 from fromfile_demod import top_block
 
-def sink_to_str(sink):
-    data = sink.data()
+def data_to_str(data):
     return ''.join(map(str, data))
 
 
 class test_samples(gr_unittest.TestCase):
-    def setUp (self):
-        self.tb = top_block('file10_5_4.13421120.dat')
-        self.sink = blocks.vector_sink_b()
-        self.tb.connect(self.tb.blocks_burst_tagger_0, self.sink)    
-
-    def tearDown (self):
-        self.tb = None
-
     def test_dat_samples(self):
-        self.tb.run()
-        self.assertTrue('1010' in sink_to_str(self.sink))
+        tb = top_block('file10_5_4.13421120.dat')
+
+        sink = blocks.vector_sink_b()
+        tb.connect(tb.blocks_burst_tagger_0, sink)    
+
+        tb.run()
+
+        self.assertTrue('1010' in data_to_str(sink.data()))
 
 if __name__ == '__main__':
     gr_unittest.run(test_samples)
